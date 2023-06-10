146231a139931aa5e1980ba99913c55129ac815c
BuildTools <unconfigured@null.spigotmc.org>
2023-06-10 12:24:19 +0200

    Audio manager freezing bug tests

--- Console Game Tests/AudioManagerTests.cs
+++ Console Game Tests/AudioManagerTests.cs
@@ -1,5 +1,6 @@
 using ConsoleGame;
 using NAudio.Wave;
+using System.Diagnostics;
 
 namespace Console_Game_Tests
 {
@@ -12,6 +13,21 @@ namespace Console_Game_Tests
                 {"sleepover", new AudioFileReader(@"D:\Users\david\Music\OMORI\003 - Lost At A Sleepover.mp3")},
                 {"angel", new AudioFileReader(@"D:\Users\david\Music\SFX\angel.mp3")}
             };
+
+        Dictionary<string, WaveStream> steps = new Dictionary<string, WaveStream>()
+        {
+            {"0", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.001.wav") },
+            {"1", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.002.wav") },
+            {"2", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.003.wav") },
+            {"3", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.004.wav") },
+            {"4", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.005.wav") },
+            {"5", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.006.wav") },
+            {"6", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.007.wav") },
+            {"7", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.008.wav") },
+            {"8", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.009.wav") },
+            {"9", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.010.wav") },
+            {"10", new AudioFileReader(@"C:\Worlds\Test\assets\sound_files\steps\Concrete.011.wav") }
+        };
 
         [TestMethod]
         public void MultipleSoundsTest()
@@ -48,12 +64,34 @@ namespace Console_Game_Tests
             Thread.Sleep(1000);
         }
 
-        //[TestMethod]
-        //public void LoopingTest()
-        //{
-        //    AudioManager audioManager = new AudioManager("sfx", "music");
+        [TestMethod]
+        public void FreezingBugTest()
+        {
+            AudioManager audioManager = new AudioManager("sfx", "music");
 
-        //    audioManager.Play("music", streams["angel"]);
-        //}
+            for(int i = 0; i < 200; i++)
+            {
+                audioManager.Play("sfx", streams["angel"]);
+                Thread.Sleep(5);
+            }
+
+            for(int i = 0; i < 500; i++)
+            {
+                KeyValuePair<string, WaveStream> kvp = steps.RandomElement();
+                audioManager.Play("sfx", kvp.Value);
+
+                int delay = Utils.rnd.Next(100, 200);
+                Thread.Sleep(delay);
+            }
+
+            for(int i = 0; i < 500; i++)
+            {
+                KeyValuePair<string, WaveStream> kvp = steps.RandomElement();
+                audioManager.Play("sfx", kvp.Value);
+
+                int delay = Utils.rnd.Next(10);
+                Thread.Sleep(delay);
+            }
+        }
     }
 }
